483faa8f3e41c77c5fb932ed26debdf218c9f960
annTerry <annak@1gb.ru>
2023-01-19 20:34:55 +0300

    feat: first try for screenshot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { mouse, left, right, up, down, Button, Point} from '@nut-tree/nut-js'; 
+import { mouse, left, right, up, down, Button, Point, screen, Region} from '@nut-tree/nut-js'; 
 import { WebSocketServer } from 'ws';
+import Jimp from 'jimp';
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -8,6 +9,39 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', data);
     const dataStr= data.toString().replace('_',' ');
     const [command, property, ...args] = dataStr.split(' ');
+    if (command === 'prnt') {
+      (async () => {const position:Point = await mouse.getPosition();  
+    const screenRegion =  new Region(position.x - 100, position.y - 100, 200, 200); 
+    const screenShot = await screen.grabRegion(screenRegion);
+    const pngScreen = new Jimp(screenShot);
+    if (pngScreen) {
+      const pngBuffer = await pngScreen.getBufferAsync(Jimp.MIME_PNG);
+      //const pngBuffer = await pngScreen.getBase64Async(Jimp.MIME_PNG);
+      console.log(`prnt_scrn ${pngBuffer}`);
+      ws.send (`prnt_scrn ${pngBuffer}`);
+    }
+    /* async (err:Error, image:any) => {
+      console.log(image);
+      if (image) {
+      const pngBufer = await image.getBase64Async( image.Jimp.MIME_PNG);
+      console.log(`prnt_scrn ${pngBufer }`);
+      ws.send (`prnt_scrn ${pngBufer }`);
+      }  
+      console.log(err);
+    }); */
+    /* Jimp.read(screenShot.data)
+  .then(image => {
+    console.log(`prnt_scrn ${image }`);
+    ws.send (`prnt_scrn ${image }`);
+  })
+  .catch(err => {
+    console.log(err);
+  }); */
+    
+  }
+)();
+    }
+    
     if (command === 'mouse') {
         (async () => {
           switch (property) {
@@ -18,7 +52,13 @@ wss.on('connection', function connection(ws) {
             case 'right' : await mouse.move(right(+args[0]));
                          break; 
             case 'down' : await mouse.move(down(+args[0]));
-                         break;                                        
+                         break;
+            case 'position': {
+                              const position:Point = await mouse.getPosition(); 
+                              console.log(`mouse_position ${position.x},${position.y}`);
+                              ws.send(`mouse_position ${position.x},${position.y}`);
+                              break;
+                              }                                                     
             default: ws.send('wrong_data');
            }
       })();
@@ -35,11 +75,9 @@ wss.on('connection', function connection(ws) {
                       circlePoints.push(firstPoint);
                       await mouse.move(circlePoints);
                       circlePoints.pop();
-                      console.log('begin', position.x, ' ', position.y);
                       for (let i=0; i < maxPoints ; i++) {
                         const x = - r + i;
                         const y = Math.round(Math.sqrt(r * r - x * x));
-                        console.log(x, ' ', y);
                         const point = new Point(position.x + x, position.y - y);
                         circlePoints.push(point);
                       }
@@ -70,7 +108,9 @@ wss.on('connection', function connection(ws) {
                        break;                                       
           default: ws.send('wrong_data');
          }
-    })();
+    }
+    
+    )();
     }
   });
   ws.send('connected');
